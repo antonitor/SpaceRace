2bc539e5c7cfa723a5c815ce4a2930aa3eb52f41
Toni <tonietorres@gmail.com>
2017-11-30 13:04:03 +0100

    multitouch

--- core/src/cat/xtec/ioc/helpers/InputHandler.java
+++ core/src/cat/xtec/ioc/helpers/InputHandler.java
@@ -20,13 +20,15 @@ public class InputHandler implements InputProcessor {
 
     private Stage stage;
 
+    // TODO EXERCICI 3 b) - Enter que enregistra el clic que mou la nau
+    private int movementPointer;
+
     public InputHandler(GameScreen screen) {
 
         // Obtenim tots els elements necessaris
         this.screen = screen;
         spacecraft = screen.getSpacecraft();
         stage = screen.getStage();
-
 
     }
 
@@ -55,18 +57,26 @@ public class InputHandler implements InputProcessor {
                 screen.setCurrentState(GameScreen.GameState.RUNNING);
                 break;
             case RUNNING:
-                previousY = screenY;
+
                 stageCoord = stage.screenToStageCoordinates(new Vector2(screenX, screenY));
                 Actor actorHit = stage.hit(stageCoord.x, stageCoord.y, true);
                 if (actorHit != null) {
-                    Gdx.app.log("HIT", actorHit.getName());
+                    Gdx.app.log("HIT", actorHit.getName() + ", pointer: " + pointer);
                     //TODO EXERCICI 2 - Si es fa clic sobre el botó pause pausem el joc
                     if (actorHit.getName().equals("pause")) {
                         screen.pauseGame();
                     //TODO EXERCICI 3 b) - Si fem clic sobre el botó fire disparem un laser
                     } else if (actorHit.getName().equals("fire")) {
                         screen.getSpacecraft().fire();
+                        // Si es toca el botó fire no volem que es tracti com a movementPointer
+                        if (movementPointer == pointer) {
+                            movementPointer = -1;
+                        }
                     }
+                } else {
+                    previousY = screenY;
+                    Gdx.app.log("HIT ", "null, pointer: " + pointer);
+                    movementPointer = pointer;
                 }
                 break;
             // Si l'estat és GameOver tornem a iniciar el joc
@@ -84,29 +94,35 @@ public class InputHandler implements InputProcessor {
     @Override
     public boolean touchUp(int screenX, int screenY, int pointer, int button) {
 
-        // Quan deixem anar el dit acabem un moviment
-        // i posem la nau en l'estat normal
-        spacecraft.goStraight();
+        // TODO EXERCICI 3 b) Tan sols si es tracta del pointer de moviment posem la nau en estat normal quan deixem anar el dit
+        if (movementPointer == pointer) {
+            spacecraft.goStraight();
+        }
         return true;
     }
 
 
     @Override
     public boolean touchDragged(int screenX, int screenY, int pointer) {
-        // Posem un umbral per evitar gestionar events quan el dit està quiet
-        if (Math.abs(previousY - screenY) > 2)
+        //TODO EXERCICI 3 b) - Tan sols  mourem la nau si es tracta del pointer de moviment
+        if (movementPointer == pointer) {
+            // Posem un umbral per evitar gestionar events quan el dit està quiet
+            if (Math.abs(previousY - screenY) > 2)
 
-            // Si la Y és major que la que tenim
-            // guardada és que va cap avall
-            if (previousY < screenY) {
-                spacecraft.goDown();
-            } else {
-                // En cas contrari cap a dalt
-                spacecraft.goUp();
-            }
-        // Guardem la posició de la Y
-        previousY = screenY;
-        return true;
+                // Si la Y és major que la que tenim
+                // guardada és que va cap avall
+                if (previousY < screenY) {
+                    spacecraft.goDown();
+                } else {
+                    // En cas contrari cap a dalt
+                    spacecraft.goUp();
+                }
+            // Guardem la posició de la Y
+            previousY = screenY;
+            return true;
+        } else {
+            return false;
+        }
     }
 
     @Override
